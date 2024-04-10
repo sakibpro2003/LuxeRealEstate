0a010ce1882752721c2bc2b0aee2e27bd79869d3
sakibpro2003 <sakibprodhan2003@gmail.com>
2024-04-10 17:24:01 +0600

    customer review card done

--- src/Customer.jsx
+++ src/Customer.jsx
@@ -21,19 +21,16 @@ function CustomerList() {
   }, []);
 
   return (
-    <div>
-      <h2>Customer List</h2>
+    <div className='mt-12'>
+      <h2 className='text-center text-6xl font-bold mb-8'>Customer List</h2>
       <ul className='grid grid-cols-3'>
         {customers.map(customer => (
           <li key={customer.id}>
             <div className="m-10 max-w-sm">
-  <div className="rounded-lg border bg-white px-4 pt-8 pb-10 shadow-lg">
-    <div className="relative mx-auto w-36 rounded-full">
-      <span className="absolute right-0 m-3 h-3 w-3 rounded-full bg-green-500 ring-2 ring-green-300 ring-offset-2"></span>
-      <img className="mx-auto h-auto w-full rounded-full" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt="" />
-    </div>
+  <div className="rounded-lg border bg-white px-4 pt-8 pb-10 shadow-2xl">
+    
     <h1 className="my-1 text-center text-xl font-bold leading-8 text-gray-900">{customer.customerName}</h1>
-    <h3 className="font-lg text-semibold text-center leading-6 text-gray-600">Marketing Exec. at Denva Corp</h3>
+    <h3 className="font-lg text-semibold text-center leading-6 text-gray-600">{customer.occupation}</h3>
     <p className="text-center text-sm leading-6 text-gray-500 hover:text-gray-600">{customer.detail_text}</p>
     <ul className="mt-3 divide-y rounded bg-gray-100 py-2 px-3 text-gray-600 shadow-sm hover:text-gray-700 hover:shadow">
       
