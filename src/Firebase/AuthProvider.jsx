97d57691b0fb408229bb0b6ab63696ea23e791bf
sakibpro2003 <sakibprodhan2003@gmail.com>
2024-04-13 23:35:39 +0600

    update profile section done

--- src/Firebase/AuthProvider.jsx
+++ src/Firebase/AuthProvider.jsx
@@ -88,17 +88,11 @@ const AuthProvider = ({ children }) => {
 
   const getUserData = () => {
     if (user !== null) {
-      // The user object has basic properties such as display name, email, etc.
       const displayName = user.displayName;
       const email = user.email;
       const photoURL = user.photoURL;
       console.log(displayName,email,photoURL)
-      // const emailVerified = user.emailVerified;
-    
-      // The user's ID, unique to the Firebase project. Do NOT use
-      // this value to authenticate with your backend server, if
-      // you have one. Use User.getToken() instead.
-      // const uid = user.uid;
+      
     }
   };
 
--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -42,7 +42,7 @@ const Register = () => {
       logOut()
         console.log(result)
         notify();
-        navigate("/");
+        // navigate("/");
       })
   };
   return (
